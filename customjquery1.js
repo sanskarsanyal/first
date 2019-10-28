3d5e3da20987a4f8cd2f609167591433a56aa658
unknown <sanskarmukul@gmail.com>
2019-10-29 00:36:27 +0530

    added my project

---
+++ customjquery1.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+document.getElementById("mainright").style.border="15px solid #FF1493";
+				
+				$(document).ready(function(){
+					
+				$('#xyz').hover(function(){
+					$(this).css('background-color','cyan');
+				})					
+				});
